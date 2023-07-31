88786102324b398c0d0a59fc02581cca64ac91b3
Deepak Jairamani <djdeepak1048@gmail.com>
2023-07-31 14:23:11 +0530

    Render of Initial UI

---
+++ src/data/data.js
@@ -0,0 +1,19 @@
+import Dessentials from '../assets/dessentials.png'
+import mcq from '../assets/aws.png'
+
+export const data = [
+  {
+    id: 1,
+    name: 'Dessentials',
+    image: Dessentials,
+    github: 'https://github.com/Sonu0012/Dessentials-Pro',
+    live: 'https://githubbox.com/Sonu0012/Dessentials-Pro',
+  },
+  {
+    id: 2,
+    name: 'MCQ Application',
+    image: mcq,
+    github: 'https://github.com/Sonu0012/mcq_app',
+    live: 'https://deepak-mcq-app.netlify.app/',
+  },
+]
